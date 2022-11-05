e106c13739b71e35c84c757b6d59833924d5e8e0
이석호 <mercen@mercen.net>
2022-11-06 02:43:00 +0900

    Initial Commit

---
+++ Allermi/AllermiApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllermiApp.swift
+//  Allermi
+//
+//  Created by Mercen on 2022/11/06.
+//
+
+import SwiftUI
+
+@main
+struct AllermiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
